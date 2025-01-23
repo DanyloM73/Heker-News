3efa6cd92055ba809a82364f5920e76374507211
DanyloM73 <danyamalanin7@gmail.com>
2025-01-23 18:07:37 +0200

    Initial Commit

---
+++ Heker News/Heker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heker_NewsApp.swift
+//  Heker News
+//
+//  Created by danylo on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Heker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Heker NewsTests/Heker_NewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Heker_NewsTests.swift
+//  Heker NewsTests
+//
+//  Created by danylo on 23.01.2025.
+//
+
+import Testing
+@testable import Heker_News
+
+struct Heker_NewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
